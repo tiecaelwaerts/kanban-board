479099112815946f1af3d38b3bbc6325b5a2e8f8
Tyler Caelwaerts <tiecaelwaerts@gmail.com>
2025-03-06 15:42:28 -0600

    auth api

--- client/src/api/authAPI.tsx
+++ client/src/api/authAPI.tsx
@@ -1,9 +1,24 @@
 import { UserLogin } from "../interfaces/UserLogin";
 
 const login = async (userInfo: UserLogin) => {
-  // TODO: make a POST request to the login route
-}
+  try {
+    const response = await fetch('/auth/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(userInfo),
+    });
 
+    if (!response.ok) {
+      throw new Error('Invalid username or password');
+    }
 
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    throw error;
+  }
+};
 
 export { login };
