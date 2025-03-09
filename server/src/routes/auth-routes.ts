4a686836792efec4dd94e6af9f8081e322afeb0a
Tyler Caelwaerts <tiecaelwaerts@gmail.com>
2025-03-09 14:06:16 -0500

    updates to auth route to add console

--- server/src/routes/auth-routes.ts
+++ server/src/routes/auth-routes.ts
@@ -5,14 +5,17 @@ import bcrypt from 'bcrypt';
 
 export const login = async (req: Request, res: Response) => {
   const { username, password } = req.body;
+  console.log(`Login attempt for username: ${username}`);
   try {
     const user = await User.findOne({ where: { username } });
     if (!user) {
+      console.log('User not found');
       return res.status(401).json({ message: 'Invalid username or password' });
     }
 
     const validPassword = await bcrypt.compare(password, user.password);
     if (!validPassword) {
+      console.log('Invalid password');
       return res.status(401).json({ message: 'Invalid username or password' });
     }
 
@@ -22,6 +25,7 @@ export const login = async (req: Request, res: Response) => {
     const token = jwt.sign({ username: user.username }, process.env.JWT_SECRET_KEY, { expiresIn: '1h' });
     return res.json({ token });
   } catch (error) {
+    console.error('Internal server error:', error);
     return res.status(500).json({ message: 'Internal server error' });
   }
 };
