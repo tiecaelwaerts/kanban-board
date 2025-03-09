434deb2f3c3804fedaf2ba9badd06cb3c8a72777
Tyler Caelwaerts <tiecaelwaerts@gmail.com>
2025-03-09 13:23:29 -0500

    update to index

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -8,7 +8,7 @@ import { TicketFactory } from './ticket.js';
 const sequelize = process.env.DB_URL
   ? new Sequelize(process.env.DB_URL)
   : new Sequelize(process.env.DB_NAME || '', process.env.DB_USER || '', process.env.DB_PASSWORD, {
-      host: 'localhost',
+      host: process.env.DB_HOST || 'localhost',
       dialect: 'postgres',
       dialectOptions: {
         decimalNumbers: true,
@@ -19,6 +19,6 @@ const User = UserFactory(sequelize);
 const Ticket = TicketFactory(sequelize);
 
 User.hasMany(Ticket, { foreignKey: 'assignedUserId' });
-Ticket.belongsTo(User, { foreignKey: 'assignedUserId', as: 'assignedUser'});
+Ticket.belongsTo(User, { foreignKey: 'assignedUserId', as: 'assignedUser' });
 
 export { sequelize, User, Ticket };
