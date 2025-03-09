4b93d724dbd5c88cd698fc202e865fb9678361f5
Tyler Caelwaerts <tiecaelwaerts@gmail.com>
2025-03-09 13:46:24 -0500

    update to index

--- server/dist/models/index.js
+++ server/dist/models/index.js
@@ -6,7 +6,7 @@ import { TicketFactory } from './ticket.js';
 const sequelize = process.env.DB_URL
     ? new Sequelize(process.env.DB_URL)
     : new Sequelize(process.env.DB_NAME || '', process.env.DB_USER || '', process.env.DB_PASSWORD, {
-        host: 'localhost',
+        host: process.env.DB_HOST || 'localhost',
         dialect: 'postgres',
         dialectOptions: {
             decimalNumbers: true,
